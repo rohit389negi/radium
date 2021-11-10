93d17bba77fa09e503b97342f4b5f90a2f8390f3
rohit389negi <rohit389negi@gmail.com>
2021-11-10 10:15:39 +0530

    assignment 2

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,4 +6,67 @@ router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
 
+router.get('/colors', function(req, res) {
+    res.send('This is another example API')
+})
+
+ router.get('/movies', function(req, res) {
+    let movie = ["suryavanshi", "bahubali", "kgf ch-2", "shershah", "sardar udham"]
+    res.send(movie)
+ })
+
+ router.get('/movies/:mex', function(req, res) {
+     let movie = ["suryavanshi", "bahubali", "kgf ch-2", "shershah", "sardar udham"]
+     let index = req.params.mex  
+     let mIndex = movie[index]
+     
+     if (index > movie.length) {
+         res.send("Invalid input")
+     } else {
+         res.send(movie[index])
+     }
+  })
+
+  router.get('/films', function(req, res) {
+    let film = [ {
+        'id': 1,
+        'name': 'The Shining'
+        }, {
+        'id': 2,
+        'name': 'Incendies'
+        }, {
+        'id': 3,
+        'name': 'Rang de Basanti'
+        }, {
+        'id': 4,
+        'name': 'Finding Nemo'
+        }]
+    res.send(film)
+ })
+
+  router.get('/films/:fex', function(req, res) {
+      let film = [ {
+        'id': 1,
+        'name': 'The Shining'
+        }, {
+        'id': 2,
+        'name': 'Incendies'
+        }, {
+        'id': 3,
+        'name': 'Rang de Basanti'
+        }, {
+        'id': 4,
+        'name': 'Finding Nemo'
+        }]
+      let indexValue = req.params.fex
+      let fIndex = film[indexValue]
+
+      if (indexValue > film.length) {
+        res.send("Invalid input")
+    } else {
+        res.send(film[indexValue])
+    }
+})
+
+
 module.exports = router;
