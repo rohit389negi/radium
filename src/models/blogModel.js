f3cd4bf60bf9cf1cba297bf1c49ce91c9a19ccca
Sachin3348 <93375005+Sachin3348@users.noreply.github.com>
2021-11-29 14:19:31 +0530

    Blogging Site Mini Project

---
+++ src/models/blogModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const ObjectId = mongoose.Schema.Types.ObjectId
+const blogSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    body: { type: String, required: true },
+    authorId: { type: ObjectId, ref: 'Author', required: true },
+    tags: [String],
+    category: { type: String, required: true },
+    subcategory: [String],
+
+    deletedAt: Date,
+    isDeleted: { type: boolean, default: false },
+    publishedAt: Date,
+    isPublished: { type: boolean, default: false }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Blog', blogSchema)
--- src/models/publisherModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose')
-
-const publisherSchema=new mongoose.Schema({
-    
-    name:String,
-    headQuater: String
-}, { timestamps: true })
-
-module.exports=mongoose.model('myPublisher',publisherSchema)
